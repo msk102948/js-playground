a505474dc73be94e8ea295b9684b3d2d60799091
hhy <msk102948@gmail.com>
2022-02-15 04:35:53 +0900

    [15-puzzle] progress 구현

--- puzzle/word.js
+++ puzzle/word.js
@@ -3,9 +3,15 @@
     let word1 = document.getElementById('word1'); // answer
     let word2 = document.getElementById('word2'); // buttons
     let check = document.getElementById('check'); // word1 == word2
+    let progress = document.getElementById('progress'); // progress check
 
     //game objects
-    let game = {'btns' : []};
+    let game = {
+      'btns' : [],
+      'max_play' : 3,
+      'current' : 0
+    };
+
     game.words = 'apple,linux,javascript,tutorial,baby,girlfriend,legend'.split(','); 
 
     //choose 1 word from words;
@@ -25,10 +31,20 @@
         this.btns.push(btn);
       }
     }
+
+    game.removeBtns = function(){
+      for(let i = 0; i < this.btns.length; i++){
+        word2.removeChild(this.btns[i]);
+      }
+      this.btns = [];
+    }
     
+    game.isCorrect = function(){
+      return this.answer === this.letters.join('');
+    }
+
     game.updateDisplay = function(){
-      let gameStr = this.letters.join('');
-      if(this.answer == gameStr){
+      if(this.isCorrect()){
         check.innerHTML = '일치합니다.';
       }else{
         check.innerHTML = '일치하지 않습니다.';
@@ -44,43 +60,71 @@
 
     game.init();
 
-
-
     game.copyBtnText = function(){
       for(let i = 0; i <this.letters.length; i++){
         this.btns[i].innerHTML = this.letters[i];
       }
     }
 
-
-// event handler
-    let swap = function(){
+    game.swap = function(){
       let temp = [];
-      while(game.letters.length !=0){
-        let s = game.letters.pop();
+      while(this.letters.length !=0){
+        let s = this.letters.pop();
         temp.push(s);
       }
-
-      game.letters = temp;
-      game.copyBtnText();
-      game.updateDisplay();
+      
+      this.letters = temp;
+      this.copyBtnText();
+      this.updateDisplay();
+    }
+    
+    game.rShift = function(){
+      let s = this.letters.pop();
+      this.letters.unshift(s);
+      
+      this.copyBtnText();
+      this.updateDisplay();
+    }
+    
+    game.lShift = function(){
+      let s = this.letters.shift();
+      this.letters.push(s);
+      
+      this.copyBtnText();
+      this.updateDisplay();
     }
 
+    game.progress = function(){
+      if(this.isCorrect()){
+        this.current++;
+        this.removeBtns();
+        this.init();
+        this.shuffle();
+        let str = "";
+        for(let i  = 0; i < game.current; i++){
+          str += "O";
+        }
+        progress.innerHTML = str;
+      }
+      if(this.current == this.max_play){
+        alert("Good! Thank you for playing!");
+      }
+    };
 
+// event handler for button
+    let swap = function(){
+      game.swap();
+      game.progress();
+    }
+    
     let rShift = function(){
-      let s = game.letters.pop();
-      game.letters.unshift(s);
-
-      game.copyBtnText();
-      game.updateDisplay();
+      game.rShift();
+      game.progress();
     }
     
     let lShift = function(){
-      let s = game.letters.shift();
-      game.letters.push(s);
-      
-      game.copyBtnText();
-      game.updateDisplay();
+      game.lShift();
+      game.progress();
     }
 
     //shuffle
@@ -89,12 +133,12 @@
       let toggle = Math.floor(Math.random() * 2) == 0;
       
       if(toggle){
-        swap();
+        game.swap();
       }
   
-      let n = Math.floor(Math.random()*game.letters.length);
+      let n = Math.floor(Math.random()*(game.letters.length -1)) ;
       for(let i =0; i<n; i++){
-        rShift();
+        game.rShift();
       }
     };
     game.shuffle();
